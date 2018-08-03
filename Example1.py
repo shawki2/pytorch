0dd5023c774aa1dde32e2fcfedff8486cc47154f
Shawki Al-Dubaee <shawki@Shawkis-MacBook-Pro.local>
2018-08-04 00:29:29 +0100

    PyTorch example

--- Example1.py
+++ Example1.py
@@ -1,3 +1,5 @@
+# https://pytorch.org/tutorials/beginner/blitz/tensor_tutorial.html#sphx-glr-beginner-blitz-tensor-tutorial-py
+
 from __future__ import print_function
 import torch
 elemEmpty = torch.empty(5, 3)
@@ -11,3 +13,13 @@ print('elemZero', elemZero)
 elemTensor = torch.tensor([5.5, 3])
 print('==========')
 print('elemTensor =', elemTensor)
+elemN = torch.tensor([5.5, 3])
+print(elemN)
+print('==========')
+# new_* methods take in sizes
+elemN = elemN.new_ones(5, 3, dtype=torch.double)
+print(elemN)
+print('==========')
+elemN = torch.randn_like(elemN, dtype=torch.float)    # override dtype!
+print(elemN)                                      # result has the same size
+print(elemN.size())
